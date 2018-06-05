4d8d768a4ce2b308782a02774b5871545850a597
Athan Clark <athan.clark@gmail.com>
2018-06-05 12:14:26 -0600

    successful build - starting deep rewrite

---
+++ src/LocalCooking/Spec/Env.purs
@@ -0,0 +1,16 @@
+module LocalCooking.Spec.Env where
+
+import LocalCooking.Common.User.Password (HashedPassword)
+import Facebook.State (FacebookLoginUnsavedFormData)
+import Facebook.Types (FacebookClientId)
+import Google.ReCaptcha (ReCaptchaSiteKey)
+
+
+
+-- FIXME why is preliminary in env? It should only exist once during boot
+type Env =
+  { development            :: Boolean
+  , facebookClientId       :: FacebookClientId
+  , googleReCaptchaSiteKey :: ReCaptchaSiteKey
+  , salt                   :: HashedPassword
+  }
