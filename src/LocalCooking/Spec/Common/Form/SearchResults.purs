03cd9797242d09fa246d6870709075003d2a8055
Athan Clark <athan.clark@gmail.com>
2018-06-18 16:03:56 -0600

    search results

---
+++ src/LocalCooking/Spec/Common/Form/SearchResults.purs
@@ -0,0 +1,110 @@
+module LocalCooking.Spec.Common.Form.SearchResults where
+
+import Prelude
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Uncurried (mkEffFn1)
+import Control.Monad.Eff.Unsafe (unsafePerformEff, unsafeCoerceEff)
+
+import Thermite as T
+import React (ReactElement, createClass, createElement) as R
+import React.DOM (div) as R
+import React.DOM.Props as RP
+import React.Queue.WhileMounted as Queue
+
+import MaterialUI.TextField (textField)
+import MaterialUI.Input as Input
+import MaterialUI.Input (inputAdornment)
+import MaterialUI.Icons.Search (searchIcon)
+
+import IxSignal.Internal (IxSignal)
+import IxSignal.Internal as IxSignal
+import Queue.Types (READ, WRITE, allowWriting, allowReading)
+import Queue.One as One
+import IxQueue (IxQueue)
+import IxQueue as IxQueue
+
+
+
+type State a =
+  { results :: Array a
+  , rerender :: Unit
+  }
+
+initialState :: forall a. {initResults :: Array a} -> State a
+initialState {initResults} =
+  { results: initResults
+  , rerender: unit
+  }
+
+data Action a
+  = SetResults (Array a)
+  | AddResults (Array a)
+  | ReRender
+
+type Effects eff =
+  ( ref :: REF
+  | eff)
+
+
+spec :: forall eff a
+      . { resultsSignal :: IxSignal (Effects eff) (Array a)
+        , renderA  :: a -> R.ReactElement
+        } -> T.Spec (Effects eff) (State a) Unit (Action a)
+spec
+  { resultsSignal
+  , renderA
+  } = T.simpleSpec performAction render
+  where
+    performAction action props state = case action of
+      SetResults xs -> do
+        liftEff (IxSignal.set xs resultsSignal)
+        void $ T.cotransform _ { results = xs }
+        performAction ReRender props state
+      AddResults xs -> do
+        ys <- liftEff $ IxSignal.get resultsSignal
+        let zs = ys <> xs
+        liftEff $ IxSignal.set zs resultsSignal
+        void $ T.cotransform _ { results = zs }
+        performAction ReRender props state
+      ReRender -> void $ T.cotransform _ { rerender = unit }
+
+    render :: T.Render (State a) Unit (Action a)
+    render dispatch props state children =
+      [ R.div
+        [ RP.style
+          { height: "10em"
+          , overflowY: "auto"
+          , padding: "0.5em"
+          }
+        ] $ map renderA state.results
+      ]
+
+
+
+results :: forall eff a
+         . { setQueue :: One.Queue (write :: WRITE) (Effects eff) (Array a)
+           , addQueue :: One.Queue (write :: WRITE) (Effects eff) (Array a)
+           , resultsSignal :: IxSignal (Effects eff) (Array a)
+           , renderA :: a -> R.ReactElement
+           } -> R.ReactElement
+results {setQueue,addQueue,resultsSignal,renderA} =
+  let init =
+        { initResults: unsafePerformEff (IxSignal.get resultsSignal)
+        }
+      {spec: reactSpec, dispatcher} =
+        T.createReactSpec
+          ( spec
+            { resultsSignal
+            , renderA
+            } ) (initialState init)
+      reactSpec' =
+          Queue.whileMountedOne
+            (allowReading setQueue)
+            (\this x -> unsafeCoerceEff $ dispatcher this $ SetResults x)
+        $ Queue.whileMountedOne
+            (allowReading addQueue)
+            (\this x -> unsafeCoerceEff $ dispatcher this $ AddResults x)
+            reactSpec
+  in  R.createElement (R.createClass reactSpec') unit []
