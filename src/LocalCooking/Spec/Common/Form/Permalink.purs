2e6b78a57078230b7add63df1bafa69eae07d813
Athan Clark <athan.clark@gmail.com>
2018-06-28 18:41:27 -0600

    permalink text field - need to bind to validate still

---
+++ src/LocalCooking/Spec/Common/Form/Permalink.purs
@@ -0,0 +1,130 @@
+module LocalCooking.Spec.Common.Form.Permalink where
+
+import Prelude
+import Data.Either (Either (..))
+import Data.String.Permalink (Permalink, permalinkParser)
+import Text.Parsing.StringParser (runParser)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Uncurried (mkEffFn1)
+import Control.Monad.Eff.Unsafe (unsafePerformEff, unsafeCoerceEff)
+
+import Thermite as T
+import React (ReactElement, createClass, createElement) as R
+import React.Queue.WhileMounted as Queue
+
+import MaterialUI.TextField (textField)
+import MaterialUI.Input as Input
+
+import Unsafe.Coerce (unsafeCoerce)
+import IxSignal.Internal (IxSignal)
+import IxSignal.Internal as IxSignal
+import Queue.Types (READ, WRITE, allowWriting, allowReading)
+import Queue.One as One
+import IxQueue (IxQueue)
+import IxQueue as IxQueue
+
+
+
+type State =
+  { permalink :: String
+  , rerender :: Unit
+  }
+
+initialState :: {initPermalink :: String} -> State
+initialState {initPermalink} =
+  { permalink: initPermalink
+  , rerender: unit
+  }
+
+data Action
+  = ChangedPermalink String
+  | SetPermalink Permalink
+  | PermalinkUnfocused
+  | ReRender
+
+type Effects eff =
+  ( ref :: REF
+  | eff)
+
+
+spec :: forall eff
+      . { permalinkSignal :: IxSignal (Effects eff) Permalink
+        , updatedQueue    :: IxQueue (read :: READ) (Effects eff) Unit
+        , label           :: R.ReactElement
+        , fullWidth       :: Boolean
+        , id              :: String
+        } -> T.Spec (Effects eff) State Unit Action
+spec
+  { permalinkSignal
+  , updatedQueue
+  , label
+  , fullWidth
+  , id
+  } = T.simpleSpec performAction render
+  where
+    performAction action props state = case action of
+      ChangedPermalink n -> do
+        void $ T.cotransform _ { permalink = n }
+        case runParser permalinkParser n of
+          Left _ -> pure unit
+          Right x -> liftEff (IxSignal.set x permalinkSignal)
+      SetPermalink x -> do
+        liftEff (IxSignal.set x permalinkSignal)
+        void $ T.cotransform _ { permalink = show x }
+        performAction ReRender props state
+        liftEff $ IxQueue.broadcastIxQueue (allowWriting updatedQueue) unit
+      PermalinkUnfocused -> do
+        case runParser permalinkParser state.permalink of
+          Left _ -> pure unit
+          Right x -> do
+            liftEff $ IxSignal.set x permalinkSignal
+            liftEff $ IxQueue.broadcastIxQueue (allowWriting updatedQueue) unit
+        performAction ReRender props state
+      ReRender -> void $ T.cotransform _ { rerender = unit }
+
+    render :: T.Render State Unit Action
+    render dispatch props state children =
+      [ textField
+        { label
+        , fullWidth
+        , value: Input.valueString (show state.permalink)
+        , onChange: mkEffFn1 \e -> dispatch $ ChangedPermalink (unsafeCoerce e).target.value
+        , onBlur: mkEffFn1 \_ -> dispatch PermalinkUnfocused
+        , name: id
+        , error: case runParser permalinkParser state.permalink of
+          Left _ -> true
+          _ -> false
+        , id
+        } []
+      ]
+
+
+
+permalink :: forall eff
+           . { label           :: R.ReactElement
+             , fullWidth       :: Boolean
+             , id              :: String
+             , updatedQueue    :: IxQueue (read :: READ) (Effects eff) Unit
+             , permalinkSignal :: IxSignal (Effects eff) Permalink
+             , setQueue        :: One.Queue (write :: WRITE) (Effects eff) Permalink
+             } -> R.ReactElement
+permalink {label,fullWidth,id,updatedQueue,permalinkSignal,setQueue} =
+  let init =
+        { initPermalink: unsafePerformEff (show <$> IxSignal.get permalinkSignal)
+        }
+      {spec: reactSpec, dispatcher} =
+        T.createReactSpec
+          ( spec
+            { label
+            , fullWidth
+            , id
+            , updatedQueue
+            , permalinkSignal
+            } ) (initialState init)
+      reactSpec' =
+          Queue.whileMountedOne
+            (allowReading setQueue)
+            (\this x -> unsafeCoerceEff $ dispatcher this $ SetPermalink x)
+            reactSpec
+  in  R.createElement (R.createClass reactSpec') unit []
